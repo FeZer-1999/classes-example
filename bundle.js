e5e810674443542dd8111e597ace978cc9b950fe
Felipe Z Saldanha <fzsaldanha@hotmail.com>
2020-07-08 22:01:16 -0300

    arrow functions exemples

--- bundle.js
+++ bundle.js
@@ -1,21 +1,26 @@
 "use strict";
 
-var arr = [1, 2, 3, 4, 5, 6]; // devolve um novo Array como resultado.
+var arr = [1, 2, 3, 4, 5, 6]; //arrow_functions
+// ----------------------
+// devolve um novo Array como resultado.
 
 var squareArr = arr.map(function (item) {
   return item * item;
 });
-console.log(squareArr); // atribui o valor de retorno ao acumulador.
+console.log(squareArr); // ----------------------
+// atribui o valor de retorno ao acumulador.
 
 var sum = arr.reduce(function (total, next) {
   return total + next;
 });
-console.log(sum); // retorna os numeros pares ou ímpares do array
+console.log(sum); // ----------------------
+// retorna os numeros pares ou ímpares do array
 
 var filter = arr.filter(function (item) {
   return item % 2 === 0;
 });
-console.log(filter); // verificar se existe uma certa info no array
+console.log(filter); // ----------------------
+// verificar se existe uma certa info no array
 
 var find = arr.find(function (item) {
   return item === 4;
--- main.js
+++ main.js
@@ -1,32 +1,32 @@
 const arr = [1, 2, 3, 4, 5, 6];
 
+//arrow_functions
+
+// ----------------------
 // devolve um novo Array como resultado.
-const squareArr = arr.map(function(item) {
-  return item * item;     
-});
+const squareArr = arr.map(item => item * item)
 
 console.log(squareArr);
 
+// ----------------------
 // atribui o valor de retorno ao acumulador.
-const sum = arr.reduce(function(total, next) {
-  return total + next;
-});
+const sum = arr.reduce((total,next) => total + next)
 
 console.log(sum);
 
+// ----------------------
 // retorna os numeros pares ou ímpares do array
-const filter = arr.filter(function(item) {
-  return item % 2 === 0;
-});
+const filter = arr.filter(item => item % 2 === 0)
 
 console.log(filter);
 
+// ----------------------
 // verificar se existe uma certa info no array
-const find = arr.find(function(item) {
-  return item === 4;
-});
+const find = arr.find(item => item === 4)
 
 console.log(find);
+
+
 
 
 
